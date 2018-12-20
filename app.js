27d19f99071f63a2aee3795dc52df34429c91a57
youngsoo shin <sys920@gmail.com>
2018-12-20 10:05:07 -0600

    changing sw.js link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 if(navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/pig_game/sw.js')
   .then(() => {
     console.log("Service Worker Installed");
   })
