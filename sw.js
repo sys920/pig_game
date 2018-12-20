16203c2473596e0f276c329a9e39f228a66646c5
youngsoo shin <sys920@gmail.com>
2018-12-20 10:44:15 -0600

    changing caches path in sw.js

--- sw.js
+++ sw.js
@@ -24,13 +24,8 @@ self.addEventListener('install', function(event) {
     caches.open(staticCache).then(function(cache) {
       return cache.addAll(
         [
-          'pig_game/',
-          'https://fonts.googleapis.com/css?family=Lato:100,300,600',
-          'https://cdn.jsdelivr.net/npm/sweetalert2@7.28.8/dist/sweetalert2.all.min.js',
-          'pig_game/index.html',
-          'pig_game/app.js',
-          'pig_game/style.css',
-          'pig_game/icons.css',  
+ 
+
           'pig_game/dice-1.png',
           'pig_game/dice-2.png',
           'pig_game/dice-3.png',
